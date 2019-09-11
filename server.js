01ac2debb73b242a8a6245644981582199a67c25
Ewelina Badowska <ewbadowska@gmail.com>
2019-09-11 22:27:08 +0200

    changing response

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 
-app.get('/', (req, res) => { res.send(db.users); })
+app.get('/', (req, res) => { res.send('it is working!') })
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
 
